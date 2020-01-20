b688d64d0297d69817c1ab810f1f0c60b40e7ee1
unknown <lzvckaya@gmail.com>
2020-01-20 16:21:42 +0900

    2020-01-20-2

---
+++ rooms/management/commands/seed_rooms.py
@@ -0,0 +1,57 @@
+from django.core.management.base import BaseCommand
+import random
+from django.contrib.admin.utils import flatten
+from django_seed import Seed
+from users import models as user_models
+from rooms import models as room_models
+
+
+class Command(BaseCommand):
+
+    help = "this command creates users"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--number", default=1, type=int, help="How many youser you want to create"
+        )
+
+    def handle(self, *args, **options):
+        number = options.get("number")
+        seeder = Seed.seeder()
+        all_users = user_models.User.objects.all()
+        room_types = room_models.RoomType.objects.all()
+
+        seeder.add_entity(
+            room_models.Room,
+            number,
+            {
+                "name": lambda x: seeder.faker.address(),
+                "host": lambda x: random.choice(all_users),
+                "room_type": lambda x: random.choice(room_types),
+                "guest": lambda x: random.randint(0, 20),
+                "price": lambda x: random.randint(0, 300),
+                "beds": lambda x: random.randint(0, 5),
+                "bedrooms": lambda x: random.randint(0, 5),
+                "baths": lambda x: random.randint(0, 5),
+            },
+        )
+        create_photos = seeder.execute()
+        created_clean = flatten(list(create_photos.values()))
+        amenities = room_models.Amenity.objects.all()
+        facilities = room_models.Facility.objects.all()
+        rules = room_models.HouserRule.objects.all()
+
+        for pk in created_clean:
+            room = room_models.Room.objects.get(pk=pk)
+            for i in range(3, random.randint(10, 17)):
+                room_models.Photo.objects.create(
+                    caption=seeder.faker.sentence(),
+                    room=room,
+                    file=f"/room_photos/{random.randint(1,33)}.webp",
+                )
+        for a in amenities:
+            magic_number=random.randint(0,15)
+            if magic_number%2==0:
+                
+        self.stdout.write(self.style.SUCCESS(f"{number} rooms created"))
+
--- users/admin.py
+++ users/admin.py
@@ -1,11 +1,22 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from . import models
+from rooms.models import Room
+
+
+class user_RoomAdmin(admin.StackedInline):
+
+    model = Room
+    extra = 0
 
 
 @admin.register(models.User)
 class CustomUserAdmin(UserAdmin):
     """Custom User Admin"""
+
+    inlines = [
+        user_RoomAdmin,
+    ]
 
     list_display = (
         "username",
@@ -18,6 +29,7 @@ class CustomUserAdmin(UserAdmin):
         "superhost",
         "is_staff",
         "is_superuser",
+        "count_rooms",
     )
 
     list_filter = UserAdmin.list_filter + ("superhost",)
@@ -37,3 +49,6 @@ class CustomUserAdmin(UserAdmin):
             },
         ),
     )
+
+    def count_rooms(self, object):
+        return object.rooms.count()
